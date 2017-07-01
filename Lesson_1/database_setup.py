78c74a65a4a0be66b12bfb544025f4337cc70e3e
Maurizio Frizio <mrz.larocca@gmail.com>
2017-07-02 01:37:25 +0200

    review lesson 1: Crud Operation with sqlalchemy

--- Lesson_1/database_setup.py
+++ Lesson_1/database_setup.py
@@ -1,7 +1,7 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 
@@ -18,12 +18,14 @@ class Restaurant(Base):
 class MenuItem(Base):
     __tablename__ = 'menu_item'
 
-    name = Column(String(80), nullable=False)
     id = Column(Integer, primary_key=True)
+
+    name = Column(String(80), nullable=False)
     description = Column(String(250))
     price = Column(String(8))
     course = Column(String(250))
     restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
+
     restaurant = relationship(Restaurant)
 
 
--- Lesson_1/lotsofmenus.py
+++ Lesson_1/lotsofmenus.py
@@ -1,14 +1,17 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from database_setup import Restaurant, Base, MenuItem
+from database_setup import Base, Restaurant, MenuItem
+
 
 engine = create_engine('sqlite:///restaurantmenu.db')
+
 # Bind the engine to the metadata of the Base class so that the
 # declaratives can be accessed through a DBSession instance
 Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
+
 # A DBSession() instance establishes all conversations with the database
 # and represents a "staging zone" for all the objects loaded into the
 # database session object. Any change made against the objects in the
@@ -377,4 +380,4 @@ session.add(menuItem1)
 session.commit()
 
 
-print "added menu items!"
+print("added menu items!")
